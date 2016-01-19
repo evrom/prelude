f581f646292cc05932e1201a3c8c911f0c9f4fa3
Evan Roman <evanroman1@gmail.com>
2016-01-19 17:24:26 +0200

    remove django web mode code

--- personal/theme.el
+++ personal/theme.el
@@ -5,8 +5,3 @@
 (add-to-list 'load-path "~/.emacs.d/elpa/moe-theme-20150918.1814/")
 (require 'moe-theme)
 (moe-light)
-
-;;use django template engine mode in webmode for all .html files
-(setq web-mode-engines-alist
-      '(("django"    . "\\.html\\'"))
-      )
